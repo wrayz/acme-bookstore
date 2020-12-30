48ce20869cdee80a4f33eea8185659132c0b4e01
WRayz <ruizhouwu@gmail.com>
2020-12-30 17:46:32 +0800

    BookDto

---
+++ aspnet-core/src/Acme.BookStore.Application.Contracts/BookDto.cs
@@ -0,0 +1,17 @@
+using System;
+using Acme.BookStore.Books;
+using Volo.Abp.Application.Dtos;
+
+namespace Acme.BookStore
+{
+    public class BookDto : AuditedEntityDto<Guid>
+    {
+        public string Name { get; set; }
+
+        public BookType Type { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public float Price { get; set; }
+    }
+}
